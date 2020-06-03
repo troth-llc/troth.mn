7320b0ffc9d9300281c68d56e2ed6dafe777ea7e
tuguldur <aa402115@gmail.com>
2020-06-03 14:24:56 +0800

    hot fix

--- src/controllers/invoice.js
+++ src/controllers/invoice.js
@@ -2,7 +2,6 @@ exports.index = (req, res) => {
   return res.json({ status: true });
 };
 exports.hook = (req, res) => {
-  console.log(req.body);
-  if (data) return res.json({ status: true });
+  if (req.body) return res.json({ status: true });
   else return res.json({ status: false });
 };
