fbd997526c474cce351183cc7469450ccef067ac
tuguldur <aa402115@gmail.com>
2020-07-02 18:51:32 +0800

    serverside fix

--- client/src/container/home/index.js
+++ client/src/container/home/index.js
@@ -50,7 +50,7 @@ const Home = () => {
   ];
   useEffect(() => {
     // if (window.screen.width >= 786) window.location.href = "/coming-soon";
-    window.location.href = "https://capstone.troth.mn?ref=troth";
+    // window.location.href = "https://capstone.troth.mn?ref=troth";
   }, []);
   return (
     <div className="d-flex flex-column container home-container">
